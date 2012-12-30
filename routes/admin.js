7ce18d3563024336d96a666e184012366cce436a
jawerty <jawerty210@gmail.com>
2012-12-30 18:09:31 -0500

    Better time printing:

--- routes/admin.js
+++ routes/admin.js
@@ -32,10 +32,18 @@ exports.new_post_handler = function(req, res){
   var year = date.getFullYear();
   var day = date.getDate();
 
-
+  function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+  }
   //organize time so it looks nice
-  var time = month + '/' + day + '/' + year + " at " + hours + ':' + minutes + ':' + seconds;
-  //get form values
+  var time = month + '/' + day + '/' + year + " at " + formatAMPM(date)
   var title = req.body.title;
   var title_sub = title.split(' ').join('-');
   
