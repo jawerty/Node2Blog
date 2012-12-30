139619f822e4d13f1a83ff623edd5f950793c1e5
jawerty <jawerty210@gmail.com>
2012-12-30 16:14:43 -0500

    Fixed iframe resize bug

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,3 +8,15 @@ function stripScripts(s) {
     }
     return div.innerHTML;
 }
+function autoResize(id){
+    var newheight;
+    var newwidth;
+
+    if(document.getElementById){
+        newheight=document.getElementById(id).contentWindow.document .body.scrollHeight;
+        newwidth=document.getElementById(id).contentWindow.document .body.scrollWidth;
+    }
+
+    document.getElementById(id).height= (newheight) + "px";
+    document.getElementById(id).width= (newwidth) + "px";
+}
