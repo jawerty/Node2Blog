91680585e5a22aa6747ef2ab6e3594f69e3b636f
Jared Wright <jawerty210@gmail.com>
2013-04-25 21:45:25 -0300

    Update db.js

--- db.js
+++ db.js
@@ -5,8 +5,8 @@ var Schema = mongoose.Schema
 
 
 //process.env.MONGOHQ_URL is for deploying on heroku
-//node2blog can be chagens to whatever you want your local database to be called i.e. 'my database'
-var db_url = process.env.MONGOHQ_URL || "mongodb://localhost:27017/your_database_name", 
+//node2blog can be changed to whatever you want your local database to be called i.e. 'my database'
+var db_url = process.env.MONGOHQ_URL || "mongodb://localhost:27017/node2blog", 
     db = mongoose.connect(db_url);
 
 //The MongoDB Schema for your posts
